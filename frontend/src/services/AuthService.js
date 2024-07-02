677c345ca1d210a2165e37373fce1676a4f757e0
HawraaYounes <hawraayounes0@gmail.com>
2024-07-02 09:53:16 +0300

    added auth service functions

--- frontend/src/services/AuthService.js
+++ frontend/src/services/AuthService.js
@@ -1,12 +1,19 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/auth/';
+const API_URL = 'http://localhost:5000';
 
 const AuthService = {
   login: async (email, password) => {
-    const response = await axios.post(API_URL + 'login', { email, password });
-    console.log("ressss",response)
-    return response;
+    const response = await axios.post(`${API_URL}/auth/login`, { email, password });
+    return response.data;
+  },
+
+  logout: () => {
+    localStorage.removeItem('token');
+  },
+
+  getCurrentUser: () => {
+    return JSON.parse(localStorage.getItem('token'));
   },
 };
 
