9acba9ea3a209c88d178ae2593ae288ec316ac9a
HawraaYounes <hawraayounes0@gmail.com>
2024-07-03 15:20:53 +0300

    added nav and hero sections

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Dashboard from './pages/Dashboard';
-import LandingPage from './pages/LandingPage'; // Adjust import path as per your structure
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import LandingPage from "./pages/LandingPage"; // Adjust import path as per your structure
+
 
 const App = () => {
   return (
     <Router>
       <Routes>
-      <Route path="/" element={<LandingPage />} />
+        <Route path="/" element={<LandingPage />} />
         <Route path="/dashboard" element={<Dashboard />} />
         {/* Add more routes as needed */}
       </Routes>
--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,15 +1,28 @@
-import React from 'react';
-import Hero from '../sections/Hero';
-import Navbar from '../components/Navbar';
+import React from "react";
+import Hero from "../sections/Hero";
+import Navbar from "../components/Navbar";
+import styles from "../style";
 // Import other sections as needed
 
 const LandingPage = () => {
   return (
-    <div>
-      <Navbar/>
-      <Hero />
-      {/* Add other sections here */}
-    </div>
+    <>
+      <div className="bg-primary w-full overflow-hidden">
+        <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+          <div className={`${styles.boxWidth}`}>
+            <Navbar />
+          </div>
+        </div>
+        <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+          <div className={`${styles.boxWidth}`}>
+            <Hero />
+          </div>
+        </div>
+        <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+          <div className={`${styles.boxWidth}`}>Other sections clients cta</div>
+        </div>
+      </div>
+    </>
   );
 };
 
