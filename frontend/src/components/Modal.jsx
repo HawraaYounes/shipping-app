9b158fa7eef79184819c910167bafa3d34c27f86
HawraaYounes <hawraayounes0@gmail.com>
2024-07-03 21:33:11 +0300

    changed modal background color

--- frontend/src/components/Modal.jsx
+++ frontend/src/components/Modal.jsx
@@ -6,7 +6,7 @@ const Modal = ({ isOpen, onClose, title, children, actions }) => {
 
   return (
     <div className="fixed inset-0 flex flex-col items-center justify-center bg-black bg-opacity-50 z-10">
-      <div className="bg-white p-6 rounded-lg max-w-md w-full">
+      <div className=" p-6 rounded-lg max-w-md w-full bg-primary">
         <button className="absolute top-2 right-2 text-gray-600 hover:text-gray-800" onClick={onClose}>✖</button>
         {title && <h3 className="text-xl font-bold mb-4">{title}</h3>}
         {children}
