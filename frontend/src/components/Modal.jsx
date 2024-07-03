ca799aca62df7990689aad6b2a801d2e18a2347f
HawraaYounes <hawraayounes0@gmail.com>
2024-07-03 21:57:02 +0300

    change modal title color

--- frontend/src/components/Modal.jsx
+++ frontend/src/components/Modal.jsx
@@ -7,7 +7,7 @@ const Modal = ({ isOpen, onClose, title, children, actions }) => {
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-10">
       <div className="p-6 rounded-lg max-w-md w-full bg-primary relative">
         <button className="absolute top-2 right-2 text-white hover:text-gray-800" onClick={onClose}>✖</button>
-        {title && <h3 className="text-xl font-bold mb-4">{title}</h3>}
+        {title && <h3 className="text-xl text-white font-bold mb-4">{title}</h3>}
         {children}
         <div className="mt-4 text-right">
           {actions}
