81884d4f4df07dc75c126186c1fbc941055cd9f6
HawraaYounes <hawraayounes0@gmail.com>
2024-07-03 13:39:54 +0300

    added frontend assets

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,47 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  mode: "jit",
   theme: {
     extend: {
       colors: {
-        yellow: {
-          50: '#DCED31',
-        },
-        black: {
-          50: '#0C0A0A',
-          
-        },
-        gray: {
-          30: '#F3EFF5',
-          50: '#D2D2D2',
-          90: '#454955',
-        },
-        orange: {
-          50: '#FF814C',
-        },
+        primary: "#00040f",
+        secondary: "#00f6ff",
+        dimWhite: "rgba(255, 255, 255, 0.7)",
+        dimBlue: "rgba(9, 151, 124, 0.1)",
       },
-      backgroundImage: {
-        'bg-img-1': "url('/img-1.png')",
-        'bg-img-2': "url('/img-2.png')",
-        'feature-bg': "url('/feature-bg.png')",
-        pattern: "url('/pattern.png')",
-        'pattern-2': "url('/pattern-bg.png')",
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
       },
-      screens: {
-        xs: '400px',
-        '3xl': '1680px',
-        '4xl': '2200px',
-      },
-      maxWidth: {
-        '10xl': '1512px',
-      },
-      borderRadius: {
-        '5xl': '40px',
-      },
+    },
+    screens: {
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px",
     },
   },
   plugins: [],
