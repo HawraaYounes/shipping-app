8587e7e788266b4b5daba62a8af3aa92812486b9
HawraaYounes <hawraayounes0@gmail.com>
2024-07-03 21:53:12 +0300

    fixing modal cancel button

--- frontend/src/components/Input.jsx
+++ frontend/src/components/Input.jsx
@@ -8,7 +8,7 @@ const Input = ({ type, placeholder, value, onChange, required }) => {
       value={value}
       onChange={onChange}
       required={required}
-      className="bg-graydark focus:outline-none rounded-lg text-gray-200 text-sm rounded-lgblock w-full p-2.5 my-1"
+      className="bg-graydark focus:outline-none rounded-lg text-gray-100 text-sm rounded-lgblock w-full p-2.5 my-1"
     />
   );
 };
--- frontend/src/components/Modal.jsx
+++ frontend/src/components/Modal.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
-import Button from './Button'; // Adjust the import path if necessary
 
 const Modal = ({ isOpen, onClose, title, children, actions }) => {
   if (!isOpen) return null;
 
   return (
-    <div className="fixed inset-0 flex flex-col items-center justify-center bg-black bg-opacity-50 z-10">
-      <div className=" p-6 rounded-lg max-w-md w-full bg-primary">
-        <button className="absolute top-2 right-2 text-gray-600 hover:text-gray-800" onClick={onClose}>✖</button>
+    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-10">
+      <div className="p-6 rounded-lg max-w-md w-full bg-primary relative">
+        <button className="absolute top-2 right-2 text-white hover:text-gray-800" onClick={onClose}>✖</button>
         {title && <h3 className="text-xl font-bold mb-4">{title}</h3>}
         {children}
         <div className="mt-4 text-right">
