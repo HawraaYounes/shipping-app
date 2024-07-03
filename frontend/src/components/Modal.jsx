885651fb41f00161311fbc6ceac3f5eee4913d92
HawraaYounes <hawraayounes0@gmail.com>
2024-07-03 22:05:38 +0300

    fix fonts

--- frontend/src/components/Modal.jsx
+++ frontend/src/components/Modal.jsx
@@ -4,7 +4,7 @@ const Modal = ({ isOpen, onClose, title, children, actions }) => {
   if (!isOpen) return null;
 
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-10">
+    <div className="font-poppins fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-10">
       <div className="p-6 rounded-lg max-w-md w-full bg-primary relative">
         <button className="absolute top-2 right-2 text-white hover:text-gray-800" onClick={onClose}>✖</button>
         {title && <h3 className="text-xl text-white font-bold mb-4">{title}</h3>}
--- frontend/src/components/ShipmentCard.jsx
+++ frontend/src/components/ShipmentCard.jsx
@@ -4,7 +4,7 @@ import Button from './Button';
 
 const ShipmentCard = ({ shipment, onEdit, onDelete }) => {
   return (
-    <div className="shipment-card flex flex-col rounded-lg p-4 shadow " style={{ width: '260px', height: '300px' }}>
+    <div className="font-poppins shipment-card flex flex-col rounded-lg p-4 shadow my-3" style={{ width: '260px', height: '300px' }}>
       <div className="flex flex-col mb-4">
         <p className="mb-2"><strong>Waybill:</strong> {shipment.waybill}</p>
         <p className="mb-2"><strong>Name:</strong> {shipment.customerName}</p>
