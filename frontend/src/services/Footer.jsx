6a70191472ec7319eb5fa9405307881d7d1f9e46
HawraaYounes <hawraayounes0@gmail.com>
2024-07-03 18:12:41 +0300

    update footer

--- frontend/src/services/Footer.jsx
+++ frontend/src/services/Footer.jsx
@@ -4,7 +4,7 @@ import { footerLinks, socialMedia } from "../constants";
 
 const Footer = () => (
   <section className={`${styles.flexCenter} ${styles.paddingY} flex-col`}>
-    <div className={`${styles.flexStart} md:flex-row flex-col mb-8 w-full`}>
+    <div className={`${styles.flexStart} md:flex-row flex-col mb-8 w-full `}>
       <div className="flex-[1] flex flex-col justify-start mr-10">
         <img
           src={logo}
@@ -12,7 +12,7 @@ const Footer = () => (
           className="w-[266px] h-[72.14px] object-contain"
         />
         <p className={`${styles.paragraph} mt-4 max-w-[312px]`}>
-          A new way to make the payments easy, reliable and secure.
+        Revolutionizing shipping with fast, reliable, and secure deliveries.
         </p>
       </div>
 
